91c80041ec093d01094f998f6bb312278cc393ef
younoch <younoch295@outlook.com>
2025-03-13 00:18:24 +0600

    init commit

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <NuxtLayout>
+    <NuxtPage :page-key="(route) => route.fullPath" />
+  </NuxtLayout>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  app: {
+    baseURL: '/portfolio/', // Replace 'portfolio' with your repo name
+  },
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true }
+})
