6068f4530cefecf8ccab9d5e38ec0261bef44006
younoch <younoch295@outlook.com>
2025-04-11 21:39:10 +0600

    Update portfolio

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   app: {
     baseURL: '/portfolio/', // Replace 'portfolio' with your repo name
+    buildAssetsDir: 'assets'
   },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true }
